0c88b0ddd9d2efb8d075a060de241ae912bc4be7
paisbet256-cloud <paisbet256@gmail.com>
2026-06-23 08:38:20 +0600

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
   nitro: {
-    preset: "cloudflare",  // 
+    preset: "cloudflare",
   },
 });
